aa8b303eba94f76cbd3b72a385ba646dbaed8e07
Phuc Nguyen <anicedream2002@gmail.com>
2022-01-29 12:34:48 -0600

    feat: save current UI

---
+++ src/components/Landing/Services/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import './index.css'
+
+export default function Services() {
+    return (
+        <div id="Services">
+            <div id="wrap">
+                <div class="cont" id="title">
+                        WHAT I DO
+                </div>
+                <div class="cont" id="subtitle">
+                    The services I offer
+                </div>
+                <div class="cont" id="grid">
+                    <div class="grid-item">
+                        <h3>Web Development</h3>
+                        <p>abc</p>
+                    </div>
+                    <div class="grid-item">
+                        <h3>App Development</h3>
+                        <p>abc</p>
+                    </div>
+                    <div class="grid-item">
+                        <h3>Teamwork</h3>
+                        <p>abc</p>
+                    </div>
+                </div>
+            </div>
+                
+
+        </div>
+    );
+}
