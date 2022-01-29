e31cea23f053546557ce6b2510d7b8e36dcde44d
Phuc Nguyen <anicedream2002@gmail.com>
2022-01-29 09:44:25 -0600

    feat: Add social icons / avatar / desc in first section

--- src/components/Landing/Biography/index.js
+++ src/components/Landing/Biography/index.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-// import Avatar from '../../../../public/img/avatar.jpeg';
+import { SocialIcon } from 'react-social-icons';
+
+import "./index.css";
 
 export default function Biography() {    
     // no need to import, img src will automatically know the src is in public dir
     const avatar = "/img/avatar.jpeg";
+    const social = [
+        {name: 'Facebook',  url: "https://www.facebook.com/phillip.nguyen.2901/"}, 
+        {name: 'Instagram', url: "https://www.instagram.com/philnguyen2901/"}, 
+        {name: 'LinkedIn',  url: "https://www.linkedin.com/in/philswe/"}
+    ]
     return (
         <div id="Biography">
-            <div id="img">
+            <div class="content" id="img">
                 <img src={avatar} alt="My Image"/> 
+            </div>
+            <div class="content">
+                <h1 id="header">
+                    I am Phillip, a developer and graduate student from Vietnam 
+                </h1>
+                <div id="description"> 
+                    From a mathematics/computing background, I always have passion with problem solving and collabrating 
+                    with people, especially when it comes to web and software development. My goal is to improve every
+                    single day, to trust the process, and to land my dream job as a software developer. 
+                </div>
+                <div id="social">
+                    {social ? 
+                        <ul>
+                            <li>
+                                <SocialIcon url={social[0].url}/>
+                            </li>
+                            <li>
+                                <SocialIcon url={social[1].url}/>
+                            </li>
+                            <li>
+                                <SocialIcon url={social[2].url}/>
+                            </li>
+
+                        </ul>
+                    : null 
+                    } 
+                    
+                </div>
             </div>
         </div>
     );
