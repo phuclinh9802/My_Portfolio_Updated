c310f8068692372521fee128a32dc32629f664be
Phuc Nguyen <anicedream2002@gmail.com>
2022-01-30 14:01:08 -0600

    feat: add icons and desc for services

--- src/components/Landing/Services/index.js
+++ src/components/Landing/Services/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
+
 import './index.css'
 
 export default function Services() {
+    const app = '/img/app.png';
+    const teamwork = '/img/teamwork.png';
+    const web = '/img/web.png';
+
     return (
         <div id="Services">
             <div id="wrap">
@@ -14,16 +19,25 @@ export default function Services() {
                 </div>
                 <div class="cont" id="grid">
                     <div class="grid-item">
+                        <img src={web} alt="Web Development"/>
                         <h3>Web Development</h3>
-                        <p>abc</p>
+                        <p>Several experiences with full-stack
+                            web development using React, Angular as 
+                            Front-end, and C#, Javascript, Node.JS as Back-end
+                        </p>
                     </div>
                     <div class="grid-item">
+                        <img src={app} alt="App Development"/>
                         <h3>App Development</h3>
-                        <p>abc</p>
+                        <p>Building windows/iOS application using Python, Selenium
+                            and Tkinter </p>
                     </div>
                     <div class="grid-item">
+                        <img src={teamwork} alt="Teamwork"/>
                         <h3>Teamwork</h3>
-                        <p>abc</p>
+                        <p>Ability to collaborate with people to come up with 
+                            the best ideas and solutions to manage/solve problems
+                        </p>
                     </div>
                 </div>
             </div>
