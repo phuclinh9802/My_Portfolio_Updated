5ae8b696132e801b780ddccf8afcf7171c91e9d3
Phuc Nguyen <anicedream2002@gmail.com>
2022-01-31 01:00:20 -0600

    feat: add router feature

--- src/components/Landing/Navbar/index.js
+++ src/components/Landing/Navbar/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // import css file for styling
 import './index.css';
@@ -7,7 +8,7 @@ export default function Navbar() {
     return (
         <div id="navbar">
             <span id="title">
-                <a href="#home">Phillip</a>
+                <Link to="/">Phillip</Link>
             </span>
             <a href="#home">Home</a>
             <a href="#portfolio">Portfolio</a>
