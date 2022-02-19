40c18f800e6fb84d6decf2f5197b2386e26c8f9b
Phuc Nguyen <anicedream2002@gmail.com>
2022-02-19 14:53:47 -0600

    feat: programming icons in covid project

--- src/components/Projects/covid/index.js
+++ src/components/Projects/covid/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import Software from '../../../data/software';
+
+import './index.css';
 
 export default function Covid() {
 
 
     return (
-        <div>
-            Hello, I'm CV
+        <div className="Covid">
+            <div className="covid-wrapper">
+                <div className="title-covid" id="title-c">
+                    <h3>Covid-19 World Tracker</h3>
+                </div>
+                <div className="title-covid" id="desc-c">
+                    <p>
+                        This is a Full-stack personal project which helps users watch, research, and analyze real-time U.S. news data and World's COVID-19 data.
+                    </p>
+                </div>
+                <div className="software">
+                    <h3>A. Programming languages, framework, and software used in the project:</h3>
+                    <ul className="sw">
+                        {
+                            Software.map((s) =>
+                                <li key={s.id}>
+                                    <img className="programming" src={s.img} width="64" height="64" />
+                                </li>
+                            )
+                        }
+                    </ul>
+                </div>
+
+            </div>
         </div>
     );
 
---
+++ src/data/software.js
@@ -0,0 +1,47 @@
+
+const Software = [
+    {
+        id: 1,
+        name: 'Python',
+        img: '/img/software_icons/python.png'
+
+    },
+    {
+        id: 2,
+        name: 'MySQL',
+        img: '/img/software_icons/mysql.png'
+
+    },
+    {
+        id: 3,
+        name: 'C#',
+        img: '/img/software_icons/csharp.png'
+
+    },
+    {
+        id: 4,
+        name: 'Angular',
+        img: '/img/software_icons/angular.png'
+
+    },
+    {
+        id: 5,
+        name: 'Postman',
+        img: '/img/software_icons/postman.png'
+
+    },
+    {
+        id: 6,
+        name: 'GitHub',
+        img: '/img/software_icons/github.png'
+
+    },
+    {
+        id: 7,
+        name: 'AWS',
+        img: '/img/software_icons/aws.png'
+
+    }
+]
+
+export default Software;
